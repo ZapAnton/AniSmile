2f53a436cad0e2f4a820134413263177e1fbaa5d
ZapAnton <wc3ft2@gmail.com>
2016-09-15 09:56:35 +0300

    Made a static smile face

--- smile.pas
+++ smile.pas
@@ -1,9 +1,13 @@
 program Smile;
 
-var
-  a : Integer;
+uses graphabc;
 
 begin
-  a := 123;
-  Writeln('Hello world ', a);
+  SetWindowSize(600, 500);
+  SetWindowTitle('AniSmile');
+  SetBrushColor(clYellow);
+  Circle(300, 250, 120);
+  SetBrushColor(clBlack);
+  Circle(250, 210, 20);
+  Circle(250, 210, 20);
 end.
