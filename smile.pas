c30c7a4ec66cd3e1386d676d9d2b15af84ba1054
ZapAnton <wc3ft2@gmail.com>
2016-09-15 10:10:27 +0300

    Added smile nose and comments

--- smile.pas
+++ smile.pas
@@ -6,26 +6,37 @@ var
   mouthCurveArray: array of Point;
 
 begin
+  {Устанавливаем параметры окна программы}
   SetWindowSize(600, 500);
   SetWindowTitle('AniSmile');
+  
+  {Начинаем рисовать смайл}
+  
+  {Рисуем тело смайла}
   SetBrushColor(clYellow);
   Circle(300, 250, 120);
+  
+  {Рисуем глаза смайла}
   SetBrushColor(clBlack);
   Circle(250, 210, 20);
   Circle(350, 210, 20);
   
+  {Рисуем нос смайла}
+  Circle(300, 270, 15);
+  
+  {Рисуем рот смайла}
   SetLength(mouthCurveArray, 4);
   
-  mouthCurveArray[0].X := 200;
+  mouthCurveArray[0].X := 210;
   mouthCurveArray[0].Y := 280;
   
-  mouthCurveArray[1].X := 300;
+  mouthCurveArray[1].X := 270;
   mouthCurveArray[1].Y := 320;
   
-  mouthCurveArray[2].X := 300;
+  mouthCurveArray[2].X := 330;
   mouthCurveArray[2].Y := 320; 
     
-  mouthCurveArray[3].X := 400;
+  mouthCurveArray[3].X := 390;
   mouthCurveArray[3].Y := 280;
   
   Curve(mouthCurveArray);
